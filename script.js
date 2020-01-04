897ca0a660ae3ef33c5d924a6bd400023d6a3797
cdROma <mrv24ru@yandex.ru>
2020-01-04 16:30:21 +0300

    update button

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 window.onload = () => {
     setTimeout(() => {
-        var openBox = document.querySelectorAll('#voice')
-        var ul = openBox[1].querySelector('ul')
-        var button = `<li id='button' style="background-color: green; "><a href="#titleSeries" style="color: #fff">Показать без рекл.</a></li>`
-        ul.innerHTML += button
-        var newButton = document.querySelector('#button')
-        var pjsdiv = document.querySelector('pjsdiv')
-        var video = pjsdiv.querySelector('video')
+        var screenWindow = document.querySelector('#MT_overroll');
+        var newScreen = '<div id="button" style="width: 100%;height: 100%; z-index: 1009;  position: absolute; top: 0; bottom: 0;"></div>';
+        screenWindow.innerHTML += newScreen;
+        var newButton = document.querySelector('#button');
+        var pjsdiv = document.querySelector('pjsdiv');
+        var video = pjsdiv.querySelector('video');
+        var openBox = document.querySelectorAll('#voice');
+        var ul = openBox[1].querySelector('ul');
+        var button = document.createElement('li')
+        button.id = 'download';
+        button.style = 'background-color: green;';
+        button.innerHTML += `<a href=${video.src} style="color: #fff">Скачать</a>`;
+        ul.prepend(button);
         console.log('video', video.src);
-        newButton.addEventListener('click', () => openFullScreen(video))
+        newButton.addEventListener('click', () => openFullScreen(video));
+        var overleyContent = document.querySelector('#overley-content');
+        overleyContent.parentNode.removeChild(overleyContent);
     }, 2000);
 }
 function openFullScreen(video) {
